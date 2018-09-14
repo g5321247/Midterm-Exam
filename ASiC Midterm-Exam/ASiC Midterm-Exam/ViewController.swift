144bb4e01670892235f1d18599a7257dd3372484
GeorgeLIu <g5321247@gmail.com>
2018-09-14 10:00:31 +0800

    set the status bar and navigation bar

---
+++ ASiC Midterm-Exam/ASiC Midterm-Exam/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ASiC Midterm-Exam
+//
+//  Created by George Liu on 2018/9/14.
+//  Copyright © 2018 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
