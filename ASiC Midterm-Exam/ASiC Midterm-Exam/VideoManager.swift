f750c3131dd5b4061901796c121c30a34871c515
GeorgeLIu <g5321247@gmail.com>
2018-09-14 11:02:20 +0800

    set video player

---
+++ ASiC Midterm-Exam/ASiC Midterm-Exam/VideoManager.swift
@@ -0,0 +1,18 @@
+//
+//  VideoManager.swift
+//  ASiC Midterm-Exam
+//
+//  Created by George Liu on 2018/9/14.
+//  Copyright © 2018 George Liu. All rights reserved.
+//
+
+import Foundation
+
+class VideoManager {
+    
+    func playVideo(address: String) {
+        
+        let url = URL(string: address)
+        
+    }
+}
--- ASiC Midterm-Exam/ASiC Midterm-Exam/ViewController.swift
+++ ASiC Midterm-Exam/ASiC Midterm-Exam/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var searchBot: UIButton!
     @IBOutlet weak var searchTxF: UITextField!
+
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    
+    @IBOutlet weak var videoView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setView()
+        playVideo(address: "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4")
+        player.play()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer.frame = videoView.bounds
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -26,7 +38,23 @@ class ViewController: UIViewController {
     @IBAction func searchBot(_ sender: UIButton) {
     }
     
-    
+    func playVideo(address: String) {
+        
+        guard let url = URL(string: address) else {
+            
+            print("URL is invalid")
+            return
+            
+        }
+        
+        player = AVPlayer(url: url)
+        
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resize
+        
+        videoView.layer.addSublayer(playerLayer)
+    }
+
     func setView() {
        
         searchBot.layer.borderWidth = 1.0
